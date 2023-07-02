50cd8c5d6cba5f04644f18bada61abcd97e1bedb
EveBaker <EveBaker0162@gmail.com>
2023-07-02 18:02:40 +0000

    got rid of extra imports on init

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.base_model import BaseModel
+from os import getenv
 
-if os.getenv('HBNB_TYPE_STORAGE') == "db":
+if getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
