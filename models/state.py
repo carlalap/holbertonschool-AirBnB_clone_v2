6e96ec5f3efd39f84f25bbe17bb6586858f5c21e
carlalap <carlalap>
2023-07-02 09:29:19 -0700

    update amenity & place files

--- models/state.py
+++ models/state.py
@@ -17,13 +17,11 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if models.storage_type == "db":
-        cities = relationship("City",
+    cities = relationship("City",
                               backref="state",
                               cascade="all, delete, delete-orphan")
 
-    else:
+    if ("HBNB_TYPE_STORAGE", None) is None:
         @property
         def cities(self):
             """
