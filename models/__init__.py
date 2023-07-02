a002dac0d8d9b78bd312158249d20fea31881827
EveBaker <EveBaker0162@gmail.com>
2023-07-02 01:31:54 +0000

    added imports for init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
 storage = FileStorage()
