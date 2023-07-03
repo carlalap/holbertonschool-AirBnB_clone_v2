89d9b5f2626d2291b5624b6ca82d374be09bdfb5
carlalap <carlalap>
2023-07-03 13:31:03 -0700

    update dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,9 @@
 -- Creates database hbnb_dev_db
 -- user hbnb_dev (localhost)
 -- password of the hbnb dev is hbnb_dev_pwd.
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
-USE hbnb_dev_db;
-
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
