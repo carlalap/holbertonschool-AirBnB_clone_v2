87870c2f10728d7af7ba2d166c77c821f0f54f02
carlalap <carlalap>
2023-07-03 13:32:27 -0700

    update test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,18 +2,9 @@
 -- A database hbnb_test_db
 -- A new user hbnb_test (in localhost)
 -- The password of hbnb_test should be set to hbnb_test_pwd
-
-CREATE DATABASE
-IF NOT EXISTS hbnb_test_db;
--- Create the user hbnb_test if it doesn't exist
-CREATE USER
-IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges to the hbnb_test user on the hbnb_test_db database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant SELECT privilege on the performance_schema database to the hbnb_test user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privileges to apply the changes
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
