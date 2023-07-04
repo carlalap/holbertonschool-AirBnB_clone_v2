df23c6ff8a34bea10a8d21633bccb3331b8bb089
EveBaker <EveBaker0162@gmail.com>
2023-07-04 17:48:19 +0000

    added get env to state

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from models.city import City
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 import models
-import os
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -17,8 +17,10 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, \
+                            delete-orphan")
 
     @property
     def cities(self):
