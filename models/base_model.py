1f08a61fd9e9487b89368016b6996815ac76359b
carlalap <carlalap>
2023-07-03 17:04:36 -0700

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -19,23 +19,27 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Instantiation a new model """
-        if "id" not in kwargs:
+        """Instantiates a new model"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
-        if "created_at" not in kwargs:
             self.created_at = datetime.now()
-        else:
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-
-        if "updated_at" not in kwargs:
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-        if "__class__" in kwargs:
-            del kwargs['__class__']
-
-        self.__dict__.update(kwargs)
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                self.updated_at = datetime.now()
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                self.created_at = datetime.now()
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -49,14 +53,11 @@ class BaseModel():
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
