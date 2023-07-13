ef073a8ec12cfef68790efb79023c4e688c5cea1
carlalap <carlalap>
2023-07-13 12:24:51 -0700

    1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 1. HBNB script that
+starts a Flask web application"""
+
+
+from flask import Flask
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
