cef2424443c5603fd5711c8ac0a2354c90f78da8
carlalap <carlalap>
2023-07-14 15:50:35 -0700

    state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -19,9 +20,7 @@ def states_list():
     """Display a HTML page with a list of all State
     objects from the DBStorage."""
     states = storage.all(State).values()
-
-
-return render_template('states_list.html', states=sorted_states)
+    return render_template('states_list.html', states=states)
 
 
 if __name__ == '__main__':
